b611e0f9d477d8ab9967527e07baf3fcde92d026
ArtysB <134217094+ArtysB@users.noreply.github.com>
2025-08-26 13:29:37 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -173,6 +173,12 @@ function toggleTab(e) {
         }
     });
 
+    // Маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]');
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
+
 })()
     
 
